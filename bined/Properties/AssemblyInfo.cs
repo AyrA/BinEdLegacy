0a3e311e3382441121ac921cce9feafdf7364cbd
Kevin Gut <kevin.gut@bitm.sg>
2018-07-25 02:33:05 +0200

    Set Application Properties

--- bined/Properties/AssemblyInfo.cs
+++ bined/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("bined")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Scripting optimized binary file editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AyrA")]
 [assembly: AssemblyProduct("bined")]
